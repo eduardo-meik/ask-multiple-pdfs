bf2433222176ac7b6a057044350f94bd31020708
eduardo-meik <eduardo@meiklabs.com>
2023-09-11 18:46:01 -0300

    lower container

--- app.py
+++ app.py
@@ -66,9 +66,11 @@ def handle_userinput(user_question):
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
 
+
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Pullm-AI, Repositorio Inteligente", page_icon=":robot_face:")
+    st.set_page_config(page_title="Pullm-AI, Repositorio Inteligente",
+                       page_icon=":robot_face:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -76,22 +78,12 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    # Create columns for layout adjustment
-    left_column, right_column = st.beta_columns([1, 3])
-
-    with right_column:
+    # Container for the Header
+    with st.beta_container():
         st.header("Asistente Virtual para consultas sobre Procedimientos")
 
-        # This will allow for scrolling and encapsulate the chat in its container
-        with st.beta_container():
-            if st.session_state.chat_history:
-                for i, message in enumerate(st.session_state.chat_history):
-                    if i % 2 == 0:
-                        st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-                    else:
-                        st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-
-    with left_column:
+    # Sidebar for documents
+    with st.sidebar:
         st.subheader("Documentos")
         pdf_docs = st.file_uploader("Cargar archivos PDF y apretar 'Procesar'", accept_multiple_files=True)
         if st.button("Procesar"):
@@ -108,10 +100,23 @@ def main():
                 # create conversation chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
 
-    # Set the user input bar at the bottom
-    user_question = st.text_input("Consultar sobre procedimientos")
-    if user_question:
-        handle_userinput(user_question)
+    # This container will occupy the remaining space
+    with st.beta_container():
+        if st.session_state.chat_history:
+            for i, message in enumerate(st.session_state.chat_history):
+                if i % 2 == 0:
+                    st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+                else:
+                    st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+    # Fixed container at the bottom for user input
+    user_input_container = st.beta_container()
+    with user_input_container:
+        user_question = st.text_input("Consultar sobre procedimientos")
+        if user_question:
+            handle_userinput(user_question)
+
 
 if __name__ == '__main__':
     main()
+
