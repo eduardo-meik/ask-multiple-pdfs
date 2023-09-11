dae9eb0827aa2a22a923abf7bf6ac93188e11119
eduardo-meik <eduardo@meiklabs.com>
2023-09-11 17:59:16 -0300

    debug

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI(temperature=0)
+    llm = ChatOpenAI()
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
