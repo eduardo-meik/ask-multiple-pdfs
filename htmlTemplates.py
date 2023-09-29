5c94b4c41776cbb3b21d730bd631f5834fb3f4dc
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2023-09-29 09:58:56 -0300

    Update htmlTemplates.py
    css update for anchor input bar

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,16 +1,17 @@
 css = '''
 <style>
+/* Your existing styles... */
 .chat-message {
     padding: 1.5rem; border-radius: 0.65rem; margin-bottom: 1rem; display: flex;
 }
 
 .chat-message.user {
-    background-color: #E3E3E3; /* Fixed color definition */
+    background-color: #E3E3E3;
     flex-direction: row-reverse;
 }
 
 .chat-message.bot {
-    background-color: #D4D4D4; /* Fixed color definition */
+    background-color: #D4D4D4;
 }
 
 .chat-message .avatar {
@@ -29,9 +30,23 @@ css = '''
   padding: 0 1.5rem;
   color: #000000;
 }
-</style>
 
+/* New styles for chat positioning */
+.chat-history {
+    max-height: calc(100vh - 120px); /* Adjust based on the estimated height of the input */
+    overflow-y: auto;
+}
+
+.chat-input {
+    position: sticky;
+    bottom: 10px;
+    background: white;
+    padding: 10px 0;
+    border-top: 1px solid #ccc;
+}
+</style>
 '''
+
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
