edbe055a950085f83947db4200987fd1af89c44a
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2024-10-10 10:32:09 -0300

    Update LLM Model app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(model_name='gpt-4')
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
